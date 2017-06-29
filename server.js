3cc67295c0d7134563a3beac2644eaebbcf0c2f0
Aaron <amiller719@gmail.com>
2017-06-28 20:28:47 -0400

    Adding delete functionality for recipes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
@@ -18,27 +17,12 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-Recipes.create('Homemade Mac and Cheese', [
-  'Ronzoni Macaroni',
-  'Sharp Cheddar Cheese',
-  'Milk',
-  'Butter'
-]);
-
-Recipes.create('Morir Soñando', [
-  'Evaporated Milk',
-  'Orange Juice',
-  'Sugar',
-  'Crushed Ice'
-]);
-
-Recipes.create('Tuna Salad', [
-  'Albacore Tuna',
-  'Eggs',
-  'Mayonaise',
-  'Relish',
-  'Onions'
-]);
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
@@ -46,9 +30,57 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i=0; i<requiredFields.length; i += 1) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe \`${req.params.id}\``);
+  res.status(204).end()
+});
+
+
+// when new recipe added, ensure has required fields. if not,
+// log error and return 400 status code with hepful message.
+// if okay, add new item, and return it with a status 201.
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i += 1) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
-});
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
